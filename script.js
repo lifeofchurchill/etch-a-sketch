95ce4c5afc8cc7f75f0187fc1725c1f6246deb77
Churchill <120373321+lifeofchurchill@users.noreply.github.com>
2025-06-14 21:52:02 +0000

    added a hover effect as well as a button which allows users to resize the gridbox to a number they want limited to not more than 100

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const container = document.querySelector('#container');
+const button = document.getElementById('resetBtn')
 
-for (let i = 0; i < 256; i++) {
+function createGridSIze(size) {
+    container.innerHTML = '';
+    const squareSize = 512 / size;
+
+    for (let i = 0; i < size * size; i++) {
     const grid = document.createElement('div');
     grid.classList.add('square')
-    container.appendChild(grid);
+    grid.style.width = `${squareSize}px`
+    grid.style.height = `${squareSize}px`
+
+    grid.addEventListener('mouseenter', () => {
+    grid.classList.add('hovered');
+    });
+    container.appendChild(grid)
+    }
 }
+
+button.addEventListener('click', () => {
+    let newSize = parseInt(prompt("Enter number of squares per side (max 100):"), 10); 
+    if (newSize && newSize > 0 && newSize <= 100) {
+        createGridSIze(newSize)
+    } else {
+        alert('Please enter a valid number between 1 and 100.');
+    }
+});
+
+createGridSIze(16);
